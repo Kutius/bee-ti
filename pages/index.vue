6d309abbbb7964be46f7cc1f70bf59777161dd73
kutius <precious.wei@qq.com>
2024-07-11 23:06:26 +0800

    chore: follow postion.x

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
 import { range } from 'radash'
+
+const { x, y } = useMouse()
+
+const progressX = computed(() => (x.value / window.innerWidth) * 100)
+const progressY = computed(() => (y.value / window.innerHeight) * 100)
 </script>
 
 <template>
   <div h-full>
-    <div flex="~ col" gap2 px-10>
-      <div v-for="i in range(5)" :key="i" h="1px" w-1 bg="white op80" />
-      <div h="1.5px" w-2 bg="white op80" />
-      <div h="1px" w-1 bg="white op80" />
+    <div flex="~ col" relative gap2 px-10>
+      <div h="2px" w-2 bg="white op80" />
+      <div v-for="i in range(10)" :key="i" h="1px" w-1 bg="white op80" />
+      <div h="2px" w-2 bg="white op80" />
+      <div v-for="i in range(10)" :key="i" h="1px" w-1 bg="white op80" />
+      <div h="2px" w-2 bg="white op80" />
+      <ClientOnly>
+        <div
+          class="i-material-symbols-play-arrow absolute left-4 translate-y--1/2"
+          transition="~ all ease-out"
+          :style="{ top: `${progressX}%` }"
+        />
+      </ClientOnly>
     </div>
   </div>
 </template>
