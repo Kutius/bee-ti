573a705f555a2a3dd73976059debfc9fdb734fd3
Kutius <precious.wei@qq.com>
2024-07-12 10:39:14 +0800

    fix: animation sticked

--- pages/index.vue
+++ pages/index.vue
@@ -16,11 +16,12 @@ const progressY = computed(() => (y.value / window.innerHeight) * 100)
       <div v-for="i in range(10)" :key="i" h="1px" w-1 bg="white op80" />
       <div h="2px" w-2 bg="white op80" />
       <ClientOnly>
-        <div
-          class="i-material-symbols-play-arrow absolute left-4 translate-y--1/2"
-          transition="~ all ease-out"
-          :style="{ top: `${progressX}%` }"
-        />
+        <Suspense>
+          <div
+            class="i-material-symbols-play-arrow absolute left-4 translate-y--1/2"
+            :style="{ top: `${progressX}%` }"
+          />
+        </Suspense>
       </ClientOnly>
     </div>
   </div>
